95068eb957bbda65e66bcda36cf87f311488ad89
Mikkel Sommer Kristensen <mikkelsk64@hotmail.com>
2020-11-04 20:02:33 +0100

    Added: Added a script that will automatically increment the project version number / bundle version number.

---
+++ Assets/UnityTools/Editor/AutoIncrementBundleVersion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+
+namespace UnityTools.Build
+{
+    public class AutoIncrementBundleVersion : Editor
+    {
+        [PostProcessBuild]
+        private static void IncrementBuildNumberOnBuild(BuildTarget targetPlatform, string buildPath)
+        {
+            string version = PlayerSettings.bundleVersion;
+
+            string[] versionNumbers = version.Split('.');
+
+            int number = int.Parse(versionNumbers[versionNumbers.Length - 1]) + 1;
+
+            versionNumbers[versionNumbers.Length - 1] = number.ToString();
+
+            PlayerSettings.bundleVersion = string.Join(".", versionNumbers);
+        }
+    }
+}
