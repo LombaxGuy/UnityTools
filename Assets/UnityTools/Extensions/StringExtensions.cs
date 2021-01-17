6c6ac3e92064ea15ccdc2611b6e818f9cfee6cfa
Mikkel Sommer Kristensen <mikkelsk64@hotmail.com>
2021-01-17 19:46:31 +0100

    Added: Added a StringExtension class with a few extentions in it.

---
+++ Assets/UnityTools/Extensions/StringExtensions.cs
@@ -0,0 +1,57 @@
+﻿namespace UnityTools.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string GetBetween(this string originalString, string startString, string endString)
+        {
+            int startIndex = originalString.IndexOf(startString) + startString.Length;
+            int endIndex = originalString.IndexOf(endString);
+
+            return originalString.Substring(startIndex, endIndex - startIndex);
+        }
+
+        public static string Replace(this string originalString, string newValue, params string[] stringsToReplace)
+        {
+            foreach (var item in stringsToReplace)
+            {
+                originalString = originalString.Replace(item, newValue);
+            }
+
+            return originalString;
+        }
+
+        public static string TrimStart(this string originalString, string stringToRemove)
+        {
+            if (originalString.StartsWith(stringToRemove))
+            {
+                return originalString.Remove(0, stringToRemove.Length);
+            }
+            else
+            {
+                return originalString;
+            }
+        }
+
+        public static string TrimEnd(this string originalString, string stringToRemove)
+        {
+            if (originalString.EndsWith(stringToRemove))
+            {
+                return originalString.Remove(originalString.Length - stringToRemove.Length);
+            }
+            else
+            {
+                return originalString;
+            }
+        }
+
+        public static string Trim(this string originalString, string stringToRemove)
+        {
+            string trimmedString = originalString;
+
+            trimmedString = trimmedString.TrimStart(stringToRemove);
+            trimmedString = trimmedString.TrimEnd(stringToRemove);
+
+            return trimmedString;
+        }
+    }
+}
